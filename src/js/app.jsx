f4fa7c6ee12258b68937b4695565cf4778307478
behuFTP <benjamin.dals.hughes@gmail.com>
2017-03-09 21:18:14 +0100

    touch events properly

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -8,7 +8,15 @@ import BirthdayResult from './birthdayResult.jsx';
 */
 
 
+function is_touch_device() {
+  return 'ontouchstart' in window        // works on most browsers 
+      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+};
 
+
+if(is_touch_device()){
+	document.getElementsByTagName('body')[0].classList.add('touch');	
+}
 //import weatherConditions from './most_typical_weather_conditions_copenhagen.json';
 setTimeout((function() {
 }).bind(this), 5000);
